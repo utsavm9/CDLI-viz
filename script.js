8746754560fc39cc9a847c64694101102d5d31f5
Utsav Munendra <utsavm9@gmail.com>
2019-06-22 16:20:30 +0530

    Maps using OpenStreetMaps

---
+++ script.js
@@ -0,0 +1,5 @@
+const mymap = L.map('mapid').setView([51.505, -0.09], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
