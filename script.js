5bff6dbafd7d7f9c9dd028915b21abb8efc460fc
Utsav Munendra <utsavm9@gmail.com>
2019-06-22 17:43:42 +0530

    CartoDB map

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const mymap = L.map('mapid').setView([51.505, -0.09], 6);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//More providers at https://leaflet-extras.github.io/leaflet-providers/preview/
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
 }).addTo(mymap);
